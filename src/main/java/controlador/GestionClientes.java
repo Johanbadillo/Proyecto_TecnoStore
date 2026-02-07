50bf6ff63e02a9fd62bef2746a2bfeba4081fcdb
johan <johanbadillomj@gmail.com>
2026-02-07 09:36:32 -0500

    feat: :sparkles: creation of main structure of tecno store project

---
+++ src/main/java/controlador/GestionClientes.java
@@ -0,0 +1,17 @@
+package controlador;
+
+import java.util.ArrayList;
+import modelo.Clientes;
+import modelo.persona;
+
+public interface GestionClientes {
+    void agregar_Clientes(persona p);
+
+    void actualizar_Clientes();
+
+    void eliminar_Clientes();
+
+    ArrayList<Clientes> visualizar_Clientes();
+
+    Clientes buscar(int id);
+}
